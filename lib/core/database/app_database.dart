42aebe30cd51b219cd0574c10f6911c74afe318f
sanin564 <saninthottungalhere@gmail.com>
2025-08-16 23:03:29 +0530

    localdb cofig changes

--- lib/core/database/app_database.dart
+++ lib/core/database/app_database.dart
@@ -4,6 +4,8 @@ import 'package:drift/drift.dart';
 import 'package:drift/native.dart';
 import 'package:path_provider/path_provider.dart' as p;
 import 'package:path/path.dart';
+import 'package:sqlite3/sqlite3.dart';
+import 'package:sqlite3_flutter_libs/sqlite3_flutter_libs.dart';
 
 part 'app_database.g.dart';
 
@@ -24,7 +26,18 @@ class AppDatabase extends _$AppDatabase {
     return LazyDatabase(() async {
       final dbFolder = await p.getApplicationDocumentsDirectory();
       final file = File(join(dbFolder.path, 'db.sqlite'));
+      // Also work around limitations on old Android versions
 
+      if (Platform.isAndroid) {
+        await applyWorkaroundToOpenSqlite3OnOldAndroidVersions();
+      }
+
+      // Make sqlite3 pick a more suitable location for temporary files - the
+      // one from the system may be inaccessible due to sandboxing.
+      final cachebase = (await p.getTemporaryDirectory()).path;
+      // We can't access /tmp on Android, which sqlite3 would try by default.
+      // Explicitly tell it about the correct temporary directory.
+      sqlite3.tempDirectory = cachebase;
       return NativeDatabase.createInBackground(file);
     });
   }
