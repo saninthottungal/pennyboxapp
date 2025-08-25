06666d303675466f987c9694dd3e66c2631fe1ed
sanin564 <saninthottungalhere@gmail.com>
2025-08-25 20:20:33 +0530

    fix warning with freezed

--- lib/services/database/models/transaction.model.dart
+++ lib/services/database/models/transaction.model.dart
@@ -4,7 +4,7 @@ import 'package:pennyboxapp/services/database/app_database.dart';
 part 'transaction.model.freezed.dart';
 
 @freezed
-sealed class Transaction with _$Transaction {
+abstract class Transaction with _$Transaction {
   const factory Transaction({
     required int id,
     required double amount,
