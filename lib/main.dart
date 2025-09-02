305aa6f67bf4be59e7cb0b222a98af164d88cdca
sanin564 <saninthottungalhere@gmail.com>
2025-09-03 00:12:55 +0530

    use system theme

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:pennyboxapp/core/theme/app_theme.dart';
 import 'package:pennyboxapp/pages/shell/shell.page.dart';
-import 'package:pennyboxapp/providers/theme_mode.provider.dart';
 import 'package:shadcn_ui/shadcn_ui.dart';
 
 void main() {
@@ -20,24 +19,20 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return GestureDetector(
       onTap: () => FocusManager.instance.primaryFocus?.unfocus(),
-      child: Consumer(
-        builder: (context, ref, child) {
-          final isDarkMode = ref.watch(isDarkThemepod);
-
-          return ShadApp(
-            theme: AppTheme.getThemeData(isDarkMode: isDarkMode),
-            home: Builder(
-              builder: (context) {
-                return ScrollConfiguration(
-                  behavior: ScrollConfiguration.of(context).copyWith(
-                    scrollbars: false,
-                  ),
-                  child: const ShellPage(),
-                );
-              },
-            ),
-          );
-        },
+      child: ShadApp(
+        theme: AppTheme.getThemeData(isDarkMode: false),
+        darkTheme: AppTheme.getThemeData(isDarkMode: true),
+        themeMode: ThemeMode.system,
+        home: Builder(
+          builder: (context) {
+            return ScrollConfiguration(
+              behavior: ScrollConfiguration.of(context).copyWith(
+                scrollbars: false,
+              ),
+              child: const ShellPage(),
+            );
+          },
+        ),
       ),
     );
   }
--- lib/pages/profile/profile.page.dart
+++ lib/pages/profile/profile.page.dart
@@ -1,36 +1,34 @@
 import 'package:flutter/material.dart';
-import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:pennyboxapp/core/constants/ui_conts.dart';
-import 'package:pennyboxapp/core/utils/context.utils.dart';
-import 'package:pennyboxapp/providers/theme_mode.provider.dart';
-import 'package:shadcn_ui/shadcn_ui.dart';
 
 class ProfilePage extends StatelessWidget {
   const ProfilePage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Center(
+    return const Center(
       child: Padding(
         padding: UiConsts.bodyHorizPadding,
         child: Row(
           mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: [
-            Text(
-              "Dark Mode",
-              style: context.textTheme.titleLarge,
-            ),
 
-            Consumer(
-              builder: (context, ref, child) {
-                final isDarkMode = ref.watch(isDarkThemepod);
-                return ShadSwitch(
-                  value: isDarkMode,
-                  onChanged: ref.read(isDarkThemepod.notifier).toggle,
-                );
-              },
-            ),
-          ],
+          ///* will be implemented later - May be -
+          // children: [
+          //   Text(
+          //     "Dark Mode",
+          //     style: context.textTheme.titleLarge,
+          //   ),
+
+          //   Consumer(
+          //     builder: (context, ref, child) {
+          //       final isDarkMode = ref.watch(isDarkThemepod);
+          //       return ShadSwitch(
+          //         value: isDarkMode,
+          //         onChanged: ref.read(isDarkThemepod.notifier).toggle,
+          //       );
+          //     },
+          //   ),
+          // ],
         ),
       ),
     );
