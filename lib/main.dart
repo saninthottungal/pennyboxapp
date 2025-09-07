31c2166a59fa83a5d6ea4668252b7622c1a50aca
sanin564 <saninthottungalhere@gmail.com>
2025-09-07 21:54:23 +0530

    native splash added - Android 12+ pending

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,19 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_native_splash/flutter_native_splash.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:pennyboxapp/core/theme/app_theme.dart';
 import 'package:pennyboxapp/pages/shell/shell.page.dart';
 import 'package:shadcn_ui/shadcn_ui.dart';
 
 void main() {
-  WidgetsFlutterBinding.ensureInitialized();
+  final binding = WidgetsFlutterBinding.ensureInitialized();
+  FlutterNativeSplash.preserve(widgetsBinding: binding);
 
   runApp(
     const ProviderScope(child: MyApp()),
   );
+
+  FlutterNativeSplash.remove();
 }
 
 class MyApp extends StatelessWidget {
