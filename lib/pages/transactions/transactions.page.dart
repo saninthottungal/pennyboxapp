d479f276879be13ab21412e0762668982189789b
sanin564 <saninthottungalhere@gmail.com>
2025-09-07 17:13:24 +0530

    setup done

--- lib/pages/transactions/transactions.page.dart
+++ lib/pages/transactions/transactions.page.dart
@@ -16,236 +16,237 @@ class TransactionsPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return DefaultTabController(
-      length: 2,
-      child: Padding(
-        padding: UiConsts.bodyHorizPadding.copyWith(
-          top: context.mdPadding.top + context.gutter,
-        ),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          spacing: context.gutter,
-          children: [
-            /// Add button
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Text(
+    return Padding(
+      padding: UiConsts.bodyHorizPadding,
+      child: DefaultTabController(
+        length: 2,
+        child: NestedScrollView(
+          headerSliverBuilder: (context, innerBoxIsScrolled) {
+            return [
+              SliverAppBar(
+                title: Text(
                   "Transactions",
                   style: context.textTheme.headlineLarge?.copyWith(
                     fontWeight: FontWeight.bold,
                   ),
                 ),
-                ShadIconButton(
-                  onPressed: () => const NewTransactionSheet().show(context),
-                  icon: const Icon(Icons.add),
-                ),
-              ],
-            ),
-            Row(
-              spacing: context.gutter,
-              children: const [
-                Expanded(
-                  child: ShadInput(
-                    placeholder: Text("Search Transactions"),
-                    leading: Icon(Icons.search),
-                  ),
-                ),
-
-                ShadIconButton.outline(
-                  icon: Icon(Icons.filter_list),
-                ),
-              ],
-            ),
-
-            DecoratedBox(
-              decoration: ShapeDecoration(
-                shape: UiConsts.shapeBoder,
-                color: context.colorScheme.secondary,
-              ),
-              child: TabBar(
-                tabs: const [
-                  Tab(
-                    text: 'History',
-                  ),
-                  Tab(
-                    text: 'Upcoming',
-                  ),
-                ],
-                dividerColor: Colors.transparent,
-                dividerHeight: 0,
-                indicatorWeight: 0,
-
-                padding: EdgeInsets.zero,
-                indicator: ShapeDecoration(
-                  shape: UiConsts.shapeBoder,
-                  color: context.colorScheme.surface,
-                ),
-                indicatorSize: TabBarIndicatorSize.tab,
-                indicatorPadding: const EdgeInsetsGeometry.all(6),
-              ),
-            ),
-
-            Expanded(
-              child: TabBarView(
-                children: [
-                  /// List
-                  Consumer(
-                    builder: (context, ref, child) {
-                      final transactions = ref
-                          .watch(getTransactionspod(false))
-                          .valueOrNull;
-
-                      if (transactions == null) {
-                        return const Center(
-                          child: CircularProgressIndicator(),
-                        );
-                      }
-
-                      return ListView.separated(
-                        padding: EdgeInsets.zero,
-                        itemCount: transactions.length,
-                        separatorBuilder: (_, _) => const Divider(),
-                        itemBuilder: (context, index) {
-                          final transaction = transactions[index];
-
-                          return ListTile(
-                            //TODO: Delete should be SOFT
-                            onLongPress: () => DeleteTransactionSheet(
-                              onDelete: () => ref
-                                  .read(deleteTransactionpod.notifier)
-                                  .delete(transaction.id),
-                            ).show(context),
-                            leading: Container(
-                              decoration: ShapeDecoration(
-                                shape: UiConsts.shapeBoder,
-                                color: transaction.type.color,
-                              ),
-                              padding: const EdgeInsets.all(4),
-                              child: Icon(
-                                transaction.type.icon,
-                                size: 20,
-                              ),
-                            ),
-                            title: Text(
-                              transaction.party?.name ??
-                                  transaction.id.toString(),
-                            ),
-                            subtitle: Text.rich(
-                              overflow: TextOverflow.ellipsis,
-                              TextSpan(
-                                text: transaction.transactionAt.toSimple(),
-                                children: transaction.description != null
-                                    ? [
-                                        const TextSpan(text: '\n'),
-                                        TextSpan(text: transaction.description),
-                                      ]
-                                    : [],
-                              ),
-                              maxLines: 3,
-                              style: context.textTheme.bodySmall?.copyWith(
-                                fontWeight: FontWeight.w600,
-
-                                //! needs change
-                                color: context.colorScheme.primary.withValues(
-                                  alpha: 0.5,
-                                ),
-                              ),
-                            ),
-
-                            trailing: Text(
-                              transaction.amount.toMoney(),
-                              style: context.textTheme.bodyLarge?.copyWith(
-                                color: transaction.type.color,
-                                fontWeight: FontWeight.w700,
-                              ),
-                            ),
-                          );
-                        },
-                      );
-                    },
-                  ),
-
-                  /// List
-                  Consumer(
-                    builder: (context, ref, child) {
-                      final transactions = ref
-                          .watch(getTransactionspod(true))
-                          .valueOrNull;
-
-                      if (transactions == null) {
-                        return const Center(
-                          child: CircularProgressIndicator(),
-                        );
-                      }
-
-                      return ListView.separated(
-                        padding: EdgeInsets.zero,
-                        itemCount: transactions.length,
-                        separatorBuilder: (_, _) => const Divider(),
-                        itemBuilder: (context, index) {
-                          final transaction = transactions[index];
-
-                          return ListTile(
-                            //TODO: Delete should be SOFT
-                            onLongPress: () => DeleteTransactionSheet(
-                              onDelete: () => ref
-                                  .read(deleteTransactionpod.notifier)
-                                  .delete(transaction.id),
-                            ).show(context),
-                            leading: Container(
-                              decoration: ShapeDecoration(
-                                shape: UiConsts.shapeBoder,
-                                color: transaction.type.color,
-                              ),
-                              padding: const EdgeInsets.all(4),
-                              child: Icon(
-                                transaction.type.icon,
-                                size: 20,
-                              ),
-                            ),
-                            title: Text(
-                              transaction.party?.name ??
-                                  transaction.id.toString(),
-                            ),
-                            subtitle: Text.rich(
-                              overflow: TextOverflow.ellipsis,
-                              TextSpan(
-                                text: transaction.transactionAt.toSimple(),
-                                children: transaction.description != null
-                                    ? [
-                                        const TextSpan(text: '\n'),
-                                        TextSpan(text: transaction.description),
-                                      ]
-                                    : [],
-                              ),
-                              maxLines: 3,
-                              style: context.textTheme.bodySmall?.copyWith(
-                                fontWeight: FontWeight.w600,
-
-                                //! needs change
-                                color: context.colorScheme.primary.withValues(
-                                  alpha: 0.5,
-                                ),
-                              ),
-                            ),
-
-                            trailing: Text(
-                              transaction.amount.toMoney(),
-                              style: context.textTheme.bodyLarge?.copyWith(
-                                color: transaction.type.color,
-                                fontWeight: FontWeight.w700,
-                              ),
-                            ),
-                          );
-                        },
-                      );
-                    },
+                centerTitle: false,
+                titleSpacing: 0,
+                pinned: true,
+                surfaceTintColor: context.colorScheme.surface,
+                actions: [
+                  ShadIconButton(
+                    onPressed: () => const NewTransactionSheet().show(context),
+                    icon: const Icon(Icons.add),
                   ),
                 ],
               ),
-            ),
-          ],
+
+              SliverAppBar(
+                primary: false,
+                titleSpacing: 0,
+                floating: true,
+                snap: true,
+                title: Row(
+                  spacing: context.gutter,
+                  children: const [
+                    Expanded(
+                      child: ShadInput(
+                        placeholder: Text("Search Transactions"),
+                        leading: Icon(Icons.search),
+                      ),
+                    ),
+
+                    ShadIconButton.outline(
+                      icon: Icon(Icons.filter_list),
+                    ),
+                  ],
+                ),
+                surfaceTintColor: context.colorScheme.surface,
+                bottom: PreferredSize(
+                  preferredSize: const Size.fromHeight(kToolbarHeight),
+                  child: DecoratedBox(
+                    decoration: ShapeDecoration(
+                      shape: UiConsts.shapeBoder,
+                      color: context.colorScheme.secondary,
+                    ),
+                    child: TabBar(
+                      tabs: const [
+                        Tab(text: 'History'),
+                        Tab(text: 'Upcoming'),
+                      ],
+                      dividerColor: Colors.transparent,
+                      dividerHeight: 0,
+                      indicatorWeight: 0,
+
+                      padding: EdgeInsets.zero,
+                      indicator: ShapeDecoration(
+                        shape: UiConsts.shapeBoder,
+                        color: context.colorScheme.surface,
+                      ),
+                      indicatorSize: TabBarIndicatorSize.tab,
+                      indicatorPadding: const EdgeInsetsGeometry.all(6),
+                    ),
+                  ),
+                ),
+              ),
+            ];
+          },
+          body: TabBarView(
+            children: [
+              /// List
+              Consumer(
+                builder: (context, ref, child) {
+                  final transactions = ref
+                      .watch(getTransactionspod(false))
+                      .valueOrNull;
+
+                  if (transactions == null) {
+                    return const Center(
+                      child: CircularProgressIndicator(),
+                    );
+                  }
+
+                  return ListView.separated(
+                    padding: EdgeInsets.zero,
+                    itemCount: transactions.length,
+                    separatorBuilder: (_, _) => const Divider(),
+                    itemBuilder: (context, index) {
+                      final transaction = transactions[index];
+
+                      return ListTile(
+                        //TODO: Delete should be SOFT
+                        onLongPress: () => DeleteTransactionSheet(
+                          onDelete: () => ref
+                              .read(deleteTransactionpod.notifier)
+                              .delete(transaction.id),
+                        ).show(context),
+                        leading: Container(
+                          decoration: ShapeDecoration(
+                            shape: UiConsts.shapeBoder,
+                            color: transaction.type.color,
+                          ),
+                          padding: const EdgeInsets.all(4),
+                          child: Icon(
+                            transaction.type.icon,
+                            size: 20,
+                          ),
+                        ),
+                        title: Text(
+                          transaction.party?.name ?? transaction.id.toString(),
+                        ),
+                        subtitle: Text.rich(
+                          overflow: TextOverflow.ellipsis,
+                          TextSpan(
+                            text: transaction.transactionAt.toSimple(),
+                            children: transaction.description != null
+                                ? [
+                                    const TextSpan(text: '\n'),
+                                    TextSpan(text: transaction.description),
+                                  ]
+                                : [],
+                          ),
+                          maxLines: 3,
+                          style: context.textTheme.bodySmall?.copyWith(
+                            fontWeight: FontWeight.w600,
+
+                            //! needs change
+                            color: context.colorScheme.primary.withValues(
+                              alpha: 0.5,
+                            ),
+                          ),
+                        ),
+
+                        trailing: Text(
+                          transaction.amount.toMoney(),
+                          style: context.textTheme.bodyLarge?.copyWith(
+                            color: transaction.type.color,
+                            fontWeight: FontWeight.w700,
+                          ),
+                        ),
+                      );
+                    },
+                  );
+                },
+              ),
+
+              /// List
+              Consumer(
+                builder: (context, ref, child) {
+                  final transactions = ref
+                      .watch(getTransactionspod(true))
+                      .valueOrNull;
+
+                  if (transactions == null) {
+                    return const Center(
+                      child: CircularProgressIndicator(),
+                    );
+                  }
+
+                  return ListView.separated(
+                    padding: EdgeInsets.zero,
+                    itemCount: transactions.length,
+                    separatorBuilder: (_, _) => const Divider(),
+                    itemBuilder: (context, index) {
+                      final transaction = transactions[index];
+
+                      return ListTile(
+                        //TODO: Delete should be SOFT
+                        onLongPress: () => DeleteTransactionSheet(
+                          onDelete: () => ref
+                              .read(deleteTransactionpod.notifier)
+                              .delete(transaction.id),
+                        ).show(context),
+                        leading: Container(
+                          decoration: ShapeDecoration(
+                            shape: UiConsts.shapeBoder,
+                            color: transaction.type.color,
+                          ),
+                          padding: const EdgeInsets.all(4),
+                          child: Icon(
+                            transaction.type.icon,
+                            size: 20,
+                          ),
+                        ),
+                        title: Text(
+                          transaction.party?.name ?? transaction.id.toString(),
+                        ),
+                        subtitle: Text.rich(
+                          overflow: TextOverflow.ellipsis,
+                          TextSpan(
+                            text: transaction.transactionAt.toSimple(),
+                            children: transaction.description != null
+                                ? [
+                                    const TextSpan(text: '\n'),
+                                    TextSpan(text: transaction.description),
+                                  ]
+                                : [],
+                          ),
+                          maxLines: 3,
+                          style: context.textTheme.bodySmall?.copyWith(
+                            fontWeight: FontWeight.w600,
+
+                            //! needs change
+                            color: context.colorScheme.primary.withValues(
+                              alpha: 0.5,
+                            ),
+                          ),
+                        ),
+
+                        trailing: Text(
+                          transaction.amount.toMoney(),
+                          style: context.textTheme.bodyLarge?.copyWith(
+                            color: transaction.type.color,
+                            fontWeight: FontWeight.w700,
+                          ),
+                        ),
+                      );
+                    },
+                  );
+                },
+              ),
+            ],
+          ),
         ),
       ),
     );
