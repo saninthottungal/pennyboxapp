d017517ae56635247120e4ceffb10c9bf490c4bc
sanin564 <saninthottungalhere@gmail.com>
2025-09-21 02:26:42 +0530

    transaction model created

---
+++ lib/services/db/models/transaction.model.dart
@@ -0,0 +1,23 @@
+import 'package:freezed_annotation/freezed_annotation.dart';
+import 'package:pennyboxapp/core/enums/transaction_type.enum.dart';
+import 'package:pennyboxapp/services/db/models/account.model.dart';
+import 'package:pennyboxapp/services/db/models/party.model.dart';
+
+part 'transaction.model.freezed.dart';
+part 'transaction.model.g.dart';
+
+@freezed
+abstract class Transaction with _$Transaction {
+  const factory Transaction({
+    required int id,
+    required double amount,
+    String? description,
+    required Account account,
+    required TxnType type,
+    required DateTime transactionAt,
+    required Party? party,
+  }) = _Transaction;
+
+  factory Transaction.fromJson(Map<String, Object?> json) =>
+      _$TransactionFromJson(json);
+}
---
+++ lib/services/db/models/transaction.model.freezed.dart
@@ -0,0 +1,337 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+// coverage:ignore-file
+// ignore_for_file: type=lint
+// ignore_for_file: unused_element, deprecated_member_use, deprecated_member_use_from_same_package, use_function_type_syntax_for_parameters, unnecessary_const, avoid_init_to_null, invalid_override_different_default_values_named, prefer_expression_function_bodies, annotate_overrides, invalid_annotation_target, unnecessary_question_mark
+
+part of 'transaction.model.dart';
+
+// **************************************************************************
+// FreezedGenerator
+// **************************************************************************
+
+// dart format off
+T _$identity<T>(T value) => value;
+
+/// @nodoc
+mixin _$Transaction {
+
+ int get id; double get amount; String? get description; Account get account; TxnType get type; DateTime get transactionAt; Party? get party;
+/// Create a copy of Transaction
+/// with the given fields replaced by the non-null parameter values.
+@JsonKey(includeFromJson: false, includeToJson: false)
+@pragma('vm:prefer-inline')
+$TransactionCopyWith<Transaction> get copyWith => _$TransactionCopyWithImpl<Transaction>(this as Transaction, _$identity);
+
+  /// Serializes this Transaction to a JSON map.
+  Map<String, dynamic> toJson();
+
+
+@override
+bool operator ==(Object other) {
+  return identical(this, other) || (other.runtimeType == runtimeType&&other is Transaction&&(identical(other.id, id) || other.id == id)&&(identical(other.amount, amount) || other.amount == amount)&&(identical(other.description, description) || other.description == description)&&(identical(other.account, account) || other.account == account)&&(identical(other.type, type) || other.type == type)&&(identical(other.transactionAt, transactionAt) || other.transactionAt == transactionAt)&&(identical(other.party, party) || other.party == party));
+}
+
+@JsonKey(includeFromJson: false, includeToJson: false)
+@override
+int get hashCode => Object.hash(runtimeType,id,amount,description,account,type,transactionAt,party);
+
+@override
+String toString() {
+  return 'Transaction(id: $id, amount: $amount, description: $description, account: $account, type: $type, transactionAt: $transactionAt, party: $party)';
+}
+
+
+}
+
+/// @nodoc
+abstract mixin class $TransactionCopyWith<$Res>  {
+  factory $TransactionCopyWith(Transaction value, $Res Function(Transaction) _then) = _$TransactionCopyWithImpl;
+@useResult
+$Res call({
+ int id, double amount, String? description, Account account, TxnType type, DateTime transactionAt, Party? party
+});
+
+
+$AccountCopyWith<$Res> get account;$PartyCopyWith<$Res>? get party;
+
+}
+/// @nodoc
+class _$TransactionCopyWithImpl<$Res>
+    implements $TransactionCopyWith<$Res> {
+  _$TransactionCopyWithImpl(this._self, this._then);
+
+  final Transaction _self;
+  final $Res Function(Transaction) _then;
+
+/// Create a copy of Transaction
+/// with the given fields replaced by the non-null parameter values.
+@pragma('vm:prefer-inline') @override $Res call({Object? id = null,Object? amount = null,Object? description = freezed,Object? account = null,Object? type = null,Object? transactionAt = null,Object? party = freezed,}) {
+  return _then(_self.copyWith(
+id: null == id ? _self.id : id // ignore: cast_nullable_to_non_nullable
+as int,amount: null == amount ? _self.amount : amount // ignore: cast_nullable_to_non_nullable
+as double,description: freezed == description ? _self.description : description // ignore: cast_nullable_to_non_nullable
+as String?,account: null == account ? _self.account : account // ignore: cast_nullable_to_non_nullable
+as Account,type: null == type ? _self.type : type // ignore: cast_nullable_to_non_nullable
+as TxnType,transactionAt: null == transactionAt ? _self.transactionAt : transactionAt // ignore: cast_nullable_to_non_nullable
+as DateTime,party: freezed == party ? _self.party : party // ignore: cast_nullable_to_non_nullable
+as Party?,
+  ));
+}
+/// Create a copy of Transaction
+/// with the given fields replaced by the non-null parameter values.
+@override
+@pragma('vm:prefer-inline')
+$AccountCopyWith<$Res> get account {
+  
+  return $AccountCopyWith<$Res>(_self.account, (value) {
+    return _then(_self.copyWith(account: value));
+  });
+}/// Create a copy of Transaction
+/// with the given fields replaced by the non-null parameter values.
+@override
+@pragma('vm:prefer-inline')
+$PartyCopyWith<$Res>? get party {
+    if (_self.party == null) {
+    return null;
+  }
+
+  return $PartyCopyWith<$Res>(_self.party!, (value) {
+    return _then(_self.copyWith(party: value));
+  });
+}
+}
+
+
+/// Adds pattern-matching-related methods to [Transaction].
+extension TransactionPatterns on Transaction {
+/// A variant of `map` that fallback to returning `orElse`.
+///
+/// It is equivalent to doing:
+/// ```dart
+/// switch (sealedClass) {
+///   case final Subclass value:
+///     return ...;
+///   case _:
+///     return orElse();
+/// }
+/// ```
+
+@optionalTypeArgs TResult maybeMap<TResult extends Object?>(TResult Function( _Transaction value)?  $default,{required TResult orElse(),}){
+final _that = this;
+switch (_that) {
+case _Transaction() when $default != null:
+return $default(_that);case _:
+  return orElse();
+
+}
+}
+/// A `switch`-like method, using callbacks.
+///
+/// Callbacks receives the raw object, upcasted.
+/// It is equivalent to doing:
+/// ```dart
+/// switch (sealedClass) {
+///   case final Subclass value:
+///     return ...;
+///   case final Subclass2 value:
+///     return ...;
+/// }
+/// ```
+
+@optionalTypeArgs TResult map<TResult extends Object?>(TResult Function( _Transaction value)  $default,){
+final _that = this;
+switch (_that) {
+case _Transaction():
+return $default(_that);case _:
+  throw StateError('Unexpected subclass');
+
+}
+}
+/// A variant of `map` that fallback to returning `null`.
+///
+/// It is equivalent to doing:
+/// ```dart
+/// switch (sealedClass) {
+///   case final Subclass value:
+///     return ...;
+///   case _:
+///     return null;
+/// }
+/// ```
+
+@optionalTypeArgs TResult? mapOrNull<TResult extends Object?>(TResult? Function( _Transaction value)?  $default,){
+final _that = this;
+switch (_that) {
+case _Transaction() when $default != null:
+return $default(_that);case _:
+  return null;
+
+}
+}
+/// A variant of `when` that fallback to an `orElse` callback.
+///
+/// It is equivalent to doing:
+/// ```dart
+/// switch (sealedClass) {
+///   case Subclass(:final field):
+///     return ...;
+///   case _:
+///     return orElse();
+/// }
+/// ```
+
+@optionalTypeArgs TResult maybeWhen<TResult extends Object?>(TResult Function( int id,  double amount,  String? description,  Account account,  TxnType type,  DateTime transactionAt,  Party? party)?  $default,{required TResult orElse(),}) {final _that = this;
+switch (_that) {
+case _Transaction() when $default != null:
+return $default(_that.id,_that.amount,_that.description,_that.account,_that.type,_that.transactionAt,_that.party);case _:
+  return orElse();
+
+}
+}
+/// A `switch`-like method, using callbacks.
+///
+/// As opposed to `map`, this offers destructuring.
+/// It is equivalent to doing:
+/// ```dart
+/// switch (sealedClass) {
+///   case Subclass(:final field):
+///     return ...;
+///   case Subclass2(:final field2):
+///     return ...;
+/// }
+/// ```
+
+@optionalTypeArgs TResult when<TResult extends Object?>(TResult Function( int id,  double amount,  String? description,  Account account,  TxnType type,  DateTime transactionAt,  Party? party)  $default,) {final _that = this;
+switch (_that) {
+case _Transaction():
+return $default(_that.id,_that.amount,_that.description,_that.account,_that.type,_that.transactionAt,_that.party);case _:
+  throw StateError('Unexpected subclass');
+
+}
+}
+/// A variant of `when` that fallback to returning `null`
+///
+/// It is equivalent to doing:
+/// ```dart
+/// switch (sealedClass) {
+///   case Subclass(:final field):
+///     return ...;
+///   case _:
+///     return null;
+/// }
+/// ```
+
+@optionalTypeArgs TResult? whenOrNull<TResult extends Object?>(TResult? Function( int id,  double amount,  String? description,  Account account,  TxnType type,  DateTime transactionAt,  Party? party)?  $default,) {final _that = this;
+switch (_that) {
+case _Transaction() when $default != null:
+return $default(_that.id,_that.amount,_that.description,_that.account,_that.type,_that.transactionAt,_that.party);case _:
+  return null;
+
+}
+}
+
+}
+
+/// @nodoc
+@JsonSerializable()
+
+class _Transaction implements Transaction {
+  const _Transaction({required this.id, required this.amount, this.description, required this.account, required this.type, required this.transactionAt, required this.party});
+  factory _Transaction.fromJson(Map<String, dynamic> json) => _$TransactionFromJson(json);
+
+@override final  int id;
+@override final  double amount;
+@override final  String? description;
+@override final  Account account;
+@override final  TxnType type;
+@override final  DateTime transactionAt;
+@override final  Party? party;
+
+/// Create a copy of Transaction
+/// with the given fields replaced by the non-null parameter values.
+@override @JsonKey(includeFromJson: false, includeToJson: false)
+@pragma('vm:prefer-inline')
+_$TransactionCopyWith<_Transaction> get copyWith => __$TransactionCopyWithImpl<_Transaction>(this, _$identity);
+
+@override
+Map<String, dynamic> toJson() {
+  return _$TransactionToJson(this, );
+}
+
+@override
+bool operator ==(Object other) {
+  return identical(this, other) || (other.runtimeType == runtimeType&&other is _Transaction&&(identical(other.id, id) || other.id == id)&&(identical(other.amount, amount) || other.amount == amount)&&(identical(other.description, description) || other.description == description)&&(identical(other.account, account) || other.account == account)&&(identical(other.type, type) || other.type == type)&&(identical(other.transactionAt, transactionAt) || other.transactionAt == transactionAt)&&(identical(other.party, party) || other.party == party));
+}
+
+@JsonKey(includeFromJson: false, includeToJson: false)
+@override
+int get hashCode => Object.hash(runtimeType,id,amount,description,account,type,transactionAt,party);
+
+@override
+String toString() {
+  return 'Transaction(id: $id, amount: $amount, description: $description, account: $account, type: $type, transactionAt: $transactionAt, party: $party)';
+}
+
+
+}
+
+/// @nodoc
+abstract mixin class _$TransactionCopyWith<$Res> implements $TransactionCopyWith<$Res> {
+  factory _$TransactionCopyWith(_Transaction value, $Res Function(_Transaction) _then) = __$TransactionCopyWithImpl;
+@override @useResult
+$Res call({
+ int id, double amount, String? description, Account account, TxnType type, DateTime transactionAt, Party? party
+});
+
+
+@override $AccountCopyWith<$Res> get account;@override $PartyCopyWith<$Res>? get party;
+
+}
+/// @nodoc
+class __$TransactionCopyWithImpl<$Res>
+    implements _$TransactionCopyWith<$Res> {
+  __$TransactionCopyWithImpl(this._self, this._then);
+
+  final _Transaction _self;
+  final $Res Function(_Transaction) _then;
+
+/// Create a copy of Transaction
+/// with the given fields replaced by the non-null parameter values.
+@override @pragma('vm:prefer-inline') $Res call({Object? id = null,Object? amount = null,Object? description = freezed,Object? account = null,Object? type = null,Object? transactionAt = null,Object? party = freezed,}) {
+  return _then(_Transaction(
+id: null == id ? _self.id : id // ignore: cast_nullable_to_non_nullable
+as int,amount: null == amount ? _self.amount : amount // ignore: cast_nullable_to_non_nullable
+as double,description: freezed == description ? _self.description : description // ignore: cast_nullable_to_non_nullable
+as String?,account: null == account ? _self.account : account // ignore: cast_nullable_to_non_nullable
+as Account,type: null == type ? _self.type : type // ignore: cast_nullable_to_non_nullable
+as TxnType,transactionAt: null == transactionAt ? _self.transactionAt : transactionAt // ignore: cast_nullable_to_non_nullable
+as DateTime,party: freezed == party ? _self.party : party // ignore: cast_nullable_to_non_nullable
+as Party?,
+  ));
+}
+
+/// Create a copy of Transaction
+/// with the given fields replaced by the non-null parameter values.
+@override
+@pragma('vm:prefer-inline')
+$AccountCopyWith<$Res> get account {
+  
+  return $AccountCopyWith<$Res>(_self.account, (value) {
+    return _then(_self.copyWith(account: value));
+  });
+}/// Create a copy of Transaction
+/// with the given fields replaced by the non-null parameter values.
+@override
+@pragma('vm:prefer-inline')
+$PartyCopyWith<$Res>? get party {
+    if (_self.party == null) {
+    return null;
+  }
+
+  return $PartyCopyWith<$Res>(_self.party!, (value) {
+    return _then(_self.copyWith(party: value));
+  });
+}
+}
+
+// dart format on
