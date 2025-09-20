32ab83e2b4abf2a70b1e969d6d4b6560d84acb90
sanin564 <saninthottungalhere@gmail.com>
2025-09-21 02:13:56 +0530

    move table creations to to db file

--- lib/services/db/db.dart
+++ lib/services/db/db.dart
@@ -33,8 +33,55 @@ class AppSqfliteDb {
       path,
       version: 1,
       onCreate: (db, version) async {
-        await transactionDao.createTables(db);
-        await transactionDao.insertDefaultTnxTypesAndAccounts(db);
+        //* Accounts Table
+        await db.execute('''
+        CREATE TABLE IF NOT EXISTS accounts
+        (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        name TEXT UNIQUE NOT NULL
+        );
+        ''');
+
+        //* Transaction Types Table
+        await db.execute('''
+        CREATE TABLE IF NOT EXISTS transaction_types
+        (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        kind TEXT UNIQUE NOT NULL
+        );
+        ''');
+
+        //* Parties Table
+        await db.execute('''
+        CREATE TABLE IF NOT EXISTS parties
+        (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        name TEXT UNIQUE NOT NULL
+        );
+        ''');
+
+        //* Transactions Table
+        await db.execute('''
+        CREATE TABLE IF NOT EXISTS transactions(
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        description TEXT,
+        amount REAL CHECK (amount > 0),
+        created_at TEXT NOT NULL  DEFAULT CURRENT_TIMESTAMP,
+        transaction_at TEXT NOT NULL DEFAULT CURRENT_TIMESTAMP,
+        account_id INTEGER NOT NULL REFERENCES accounts(id) ON DELETE CASCADE ON UPDATE CASCADE,
+        transaction_type_id INTEGER NOT NULL REFERENCES transaction_types(id) ON DELETE CASCADE ON UPDATE CASCADE,
+        party_id INTEGER  REFERENCES parties(id)  ON DELETE SET NULL ON UPDATE SET NULL
+        );
+        ''');
+
+        //* Insert default values
+        await db.rawInsert('INSERT INTO accounts (name) values ("Cash");');
+        await db.rawInsert('''
+        INSERT INTO transaction_types (kind) VALUES 
+        ("Income"),
+        ("Expense"),
+        ("Transfer");
+        ''');
       },
     );
   }
--- lib/services/db/transactions.dao.dart
+++ lib/services/db/transactions.dao.dart
@@ -1,6 +1,5 @@
 import 'package:freezed_annotation/freezed_annotation.dart';
 import 'package:pennyboxapp/services/db/db.dart';
-import 'package:sqflite/sqflite.dart';
 
 part 'transactions.dao.g.dart';
 part 'transactions.dao.freezed.dart';
@@ -20,49 +19,4 @@ class TransactionDao {
   TransactionDao(this._db);
 
   final AppSqfliteDb _db;
-
-  Future<void> createTables(Database db) {
-    return db.execute('''
-CREATE TABLE IF NOT EXISTS  accounts
-(
-id INTEGER PRIMARY KEY AUTOINCREMENT,
-name TEXT UNIQUE NOT NULL
-);
-
-CREATE TABLE IF NOT EXISTS transaction_types
-(
-id INTEGER PRIMARY KEY AUTOINCREMENT,
-kind TEXT UNIQUE NOT NULL
-);
-
-CREATE TABLE IF NOT EXISTS parties
-(
-id INTEGER PRIMARY KEY AUTOINCREMENT,
-name TEXT UNIQUE NOT NULL
-);
-
-
-CREATE TABLE IF NOT EXISTS transactions(
-id INTEGER PRIMARY KEY AUTOINCREMENT,
-description TEXT,
-amount REAL CHECK (amount > 0),
-created_at TEXT NOT NULL  DEFAULT CURRENT_TIMESTAMP,
-transaction_at TEXT NOT NULL DEFAULT CURRENT_TIMESTAMP,
-account_id INTEGER NOT NULL REFERENCES accounts(id) ON DELETE CASCADE ON UPDATE CASCADE,
-transaction_type_id INTEGER NOT NULL REFERENCES transaction_types(id) ON DELETE CASCADE ON UPDATE CASCADE,
-party_id INTEGER  REFERENCES parties(id)  ON DELETE SET NULL ON UPDATE SET NULL
-);
-''');
-  }
-
-  Future<void> insertDefaultTnxTypesAndAccounts(Database db) {
-    return db.execute(
-      '''
-INSERT INTO accounts (name) values (?);
-INSERT INTO transaction_types (kind) VALUES (?),(?),(?);
-
-    ''',
-      ["Cash", "Income", "Expense", "Transfer"],
-    );
-  }
 }
