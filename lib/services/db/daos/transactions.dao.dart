9faf27aa475f08e94156255f23097d8cb92e97ba
sanin564 <saninthottungalhere@gmail.com>
2025-09-21 02:33:38 +0530

    get accounts query added

--- lib/services/db/daos/transactions.dao.dart
+++ lib/services/db/daos/transactions.dao.dart
@@ -1,7 +1,13 @@
+import 'package:pennyboxapp/services/db/models/account.model.dart';
 import 'package:sqflite/sqflite.dart';
 
 class TransactionDao {
   TransactionDao(this._db);
 
   final Database _db;
+
+  Future<List<Account>> getAccounts() async {
+    final res = await _db.rawQuery('SELECT * FROM accounts');
+    return res.map(Account.fromJson).toList();
+  }
 }
