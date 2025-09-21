69144093bfe01ae05efbd230c3099c75c1f9e504
sanin564 <saninthottungalhere@gmail.com>
2025-09-21 17:43:50 +0530

    add transaction method added

--- lib/services/db/daos/transactions.dao.dart
+++ lib/services/db/daos/transactions.dao.dart
@@ -1,3 +1,4 @@
+import 'package:pennyboxapp/core/enums/transaction_type.enum.dart';
 import 'package:pennyboxapp/services/db/models/account.model.dart';
 import 'package:sqflite/sqflite.dart';
 
@@ -9,5 +10,55 @@ class TransactionDao {
   Future<List<Account>> getAccounts() async {
     final res = await _db.rawQuery('SELECT * FROM accounts');
     return res.map(Account.fromJson).toList();
+  }
+
+  Future<List<TxnType>> getTransactionTypes() async {
+    final res = await _db.rawQuery('SELECT * FROM transaction_types');
+
+    return res
+        .map(
+          (e) => TxnType.values.singleWhere((type) => type.id == e['id']),
+        )
+        .toList();
+  }
+
+  Future<void> addTransactions({
+    required double amount,
+    required int accountId,
+    required int transactionTypeId,
+    required DateTime? transactionAt,
+    required int partyId,
+    required String? description,
+  }) {
+    return _db.rawInsert(
+      '''
+INSERT INTO transactions 
+(
+description,
+amount,
+ transaction_at,
+ account_id, 
+ transaction_type_id,
+ party_id
+) 
+VALUES 
+(
+?,
+?,
+?,
+?,
+?,
+?
+);
+''',
+      [
+        description,
+        amount,
+        transactionAt?.toIso8601String(),
+        accountId,
+        transactionTypeId,
+        partyId,
+      ],
+    );
   }
 }
