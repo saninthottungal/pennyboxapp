5c96071afbbe11c2e3e7253f74c8bdb4c59ae8c7
sanin564 <saninthottungalhere@gmail.com>
2025-09-25 22:00:48 +0530

    event bus created

--- lib/pages/home/home.logic.dart
+++ lib/pages/home/home.logic.dart
@@ -1,18 +1,29 @@
+import 'dart:async';
+
 import 'package:flutter/cupertino.dart';
+import 'package:pennyboxapp/services/db/daos/transactions.dao.dart';
 import 'package:pennyboxapp/services/db/db.dart';
 import 'package:pennyboxapp/services/db/models/account_with_balance.model.dart';
+import 'package:pennyboxapp/services/event_bus/event_bus.dart';
 
 class AccountsLogic extends ChangeNotifier {
   AccountsLogic() {
+    _dao = AppDatabase().transactionDao;
     getAccountBalances();
+    _eventSub = eventBus.on<FetchAccountBalances>().listen(
+      (_) => getAccountBalances(),
+    );
   }
+
+  late final TransactionDao _dao;
+  StreamSubscription<FetchAccountBalances>? _eventSub;
 
   List<AccountwBalance> balances = [];
   bool isLoading = false;
 
   Future<void> getAccountBalances() async {
     isLoading = true;
-    final res = await AppDatabase().transactionDao.getAccountBalances();
+    final res = await _dao.getAccountBalances();
     balances = res;
     isLoading = false;
 
@@ -20,13 +31,19 @@ class AccountsLogic extends ChangeNotifier {
   }
 
   Future<void> addAccount(String name) async {
-    await AppDatabase().transactionDao.addAccount(name);
+    await _dao.addAccount(name);
     getAccountBalances();
   }
 
   Future<void> deleteAccount(int id) async {
-    await AppDatabase().transactionDao.deleteAccount(id);
+    await _dao.deleteAccount(id);
     getAccountBalances();
+  }
+
+  @override
+  void dispose() {
+    _eventSub?.cancel();
+    super.dispose();
   }
 }
 
