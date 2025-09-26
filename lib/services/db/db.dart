da4efa982b83b72ebb05b7a43a00333162e3e7f4
sanin564 <saninthottungalhere@gmail.com>
2025-09-26 23:10:30 +0530

    refactoring

--- lib/services/db/db.dart
+++ lib/services/db/db.dart
@@ -1,6 +1,7 @@
 import 'package:path/path.dart';
 import 'package:path_provider/path_provider.dart';
 import 'package:pennyboxapp/services/db/daos/transactions.dao.dart';
+import 'package:pennyboxapp/services/db/db_migrations.dart';
 import 'package:sqflite/sqflite.dart';
 
 class AppDatabase {
@@ -26,59 +27,16 @@ class AppDatabase {
       version: 1,
       onCreate: (db, version) async {
         final batch = db.batch();
-
-        //* Accounts Table
-        batch.execute('''
-        CREATE TABLE IF NOT EXISTS accounts
-        (
-        id INTEGER PRIMARY KEY AUTOINCREMENT,
-        name TEXT UNIQUE NOT NULL
-        );
-        ''');
-
-        //* Transaction Types Table
-        batch.execute('''
-        CREATE TABLE IF NOT EXISTS transaction_types
-        (
-        id INTEGER PRIMARY KEY AUTOINCREMENT,
-        kind TEXT UNIQUE NOT NULL
-        );
-        ''');
-
-        //* Parties Table
-        batch.execute('''
-        CREATE TABLE IF NOT EXISTS parties
-        (
-        id INTEGER PRIMARY KEY AUTOINCREMENT,
-        name TEXT UNIQUE NOT NULL
-        );
-        ''');
-
-        //* Transactions Table
-        batch.execute('''
-        CREATE TABLE IF NOT EXISTS transactions(
-        id INTEGER PRIMARY KEY AUTOINCREMENT,
-        description TEXT,
-        amount REAL CHECK (amount > 0),
-        created_at TEXT NOT NULL  DEFAULT CURRENT_TIMESTAMP,
-        transaction_at TEXT NOT NULL DEFAULT CURRENT_TIMESTAMP,
-        account_id INTEGER NOT NULL REFERENCES accounts(id) ON DELETE CASCADE ON UPDATE CASCADE,
-        transaction_type_id INTEGER NOT NULL REFERENCES transaction_types(id) ON DELETE CASCADE ON UPDATE CASCADE,
-        party_id INTEGER  REFERENCES parties(id)  ON DELETE SET NULL ON UPDATE SET NULL
-        );
-        ''');
-
-        //* Insert default values
-        batch.rawInsert('INSERT INTO accounts (name) values ("Cash");');
-        batch.rawInsert('''
-        INSERT INTO transaction_types (kind) VALUES 
-        ("Income"),
-        ("Expense"),
-        ("Transfer");
-        ''');
-
-        await batch.commit(noResult: true);
+        DbMigrations.queries[version]?.forEach(batch.execute);
+        await batch.commit();
       },
+      // onUpgrade: (db, oldVersion, newVersion) async {
+      //   for (int i = oldVersion + 1; i <= newVersion; i++) {
+      //     for (final query in DbMigrations.queries[i]!) {
+      //       await db.execute(query);
+      //     }
+      //   }
+      // },
     );
 
     transactionDao = TransactionDao(_db);
---
+++ lib/services/db/db_migrations.dart
@@ -0,0 +1,59 @@
+class DbMigrations {
+  DbMigrations._();
+
+  static const _v1 = [
+    //* Accounts Table
+    '''
+        CREATE TABLE IF NOT EXISTS accounts
+        (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        name TEXT UNIQUE NOT NULL
+        );
+        ''',
+
+    //* Transaction Types Table
+    '''
+        CREATE TABLE IF NOT EXISTS transaction_types
+        (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        kind TEXT UNIQUE NOT NULL
+        );
+        ''',
+
+    //* Parties Table
+    '''
+        CREATE TABLE IF NOT EXISTS parties
+        (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        name TEXT UNIQUE NOT NULL
+        );
+        ''',
+
+    //* Transactions Table
+    '''
+        CREATE TABLE IF NOT EXISTS transactions(
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        description TEXT,
+        amount REAL CHECK (amount > 0),
+        created_at TEXT NOT NULL  DEFAULT CURRENT_TIMESTAMP,
+        transaction_at TEXT NOT NULL DEFAULT CURRENT_TIMESTAMP,
+        account_id INTEGER NOT NULL REFERENCES accounts(id) ON DELETE CASCADE ON UPDATE CASCADE,
+        transaction_type_id INTEGER NOT NULL REFERENCES transaction_types(id) ON DELETE CASCADE ON UPDATE CASCADE,
+        party_id INTEGER  REFERENCES parties(id)  ON DELETE SET NULL ON UPDATE SET NULL
+        );
+        ''',
+
+    //* Insert default values
+    'INSERT INTO accounts (name) values ("Cash");',
+    '''
+        INSERT INTO transaction_types (kind) VALUES 
+        ("Income"),
+        ("Expense"),
+        ("Transfer");
+        ''',
+  ];
+
+  static const Map<int, List<String>> queries = {
+    1: _v1,
+  };
+}
