7ec9b6635b41f2aee4b2b7d16cbe3e3bfee75a24
sanin <saninthottungalhere@gmail.com>
2026-01-18 17:16:28 +0530

    bottom nav commented

--- lib/pages/shell/shell.page.dart
+++ lib/pages/shell/shell.page.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:pennyboxapp/core/constants/ui_conts.dart';
-import 'package:pennyboxapp/pages/home/home.page.dart';
-import 'package:pennyboxapp/pages/profile/profile.page.dart';
+// import 'package:pennyboxapp/core/constants/ui_conts.dart';
+// import 'package:pennyboxapp/pages/home/home.page.dart';
+// import 'package:pennyboxapp/pages/profile/profile.page.dart';
 import 'package:pennyboxapp/pages/transactions/transactions.page.dart';
 
 class ShellPage extends StatefulWidget {
@@ -12,53 +12,58 @@ class ShellPage extends StatefulWidget {
 }
 
 class _ShellPageState extends State<ShellPage> {
-  int _currentIndex = 0;
+  // int _currentIndex = 0;
 
-  final List<Widget> _pages = const [
-    HomePage(),
-    TransactionsPage(),
-    ProfilePage(),
-  ];
+  // final List<Widget> _pages = const [
+  //   HomePage(),
+  //   TransactionsPage(),
+  //   ProfilePage(),
+  // ];
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: IndexedStack(
-        index: _currentIndex,
-        children: _pages,
-      ),
-      // body: _pages[_currentIndex],
-      bottomNavigationBar: NavigationBar(
-        indicatorShape: UiConsts.shapeBoder,
-        selectedIndex: _currentIndex,
-        onDestinationSelected: (value) {
-          setState(() {
-            _currentIndex = value;
-          });
-        },
-        destinations: [
-          NavigationDestination(
-            icon: Icon(
-              _currentIndex == 0 ? Icons.home : Icons.home_outlined,
-            ),
-            label: "Home",
-          ),
-          NavigationDestination(
-            icon: Icon(
-              _currentIndex == 1
-                  ? Icons.receipt_rounded
-                  : Icons.receipt_outlined,
-            ),
-            label: "Transactions",
-          ),
-          NavigationDestination(
-            icon: Icon(
-              _currentIndex == 2 ? Icons.person : Icons.person_outline,
-            ),
-            label: "Profile",
-          ),
-        ],
-      ),
+    return const Scaffold(
+      body: TransactionsPage(),
     );
+
+    // ! will be uncommented later
+    // return Scaffold(
+    //   body: IndexedStack(
+    //     index: _currentIndex,
+    //     children: _pages,
+    //   ),
+    //   // body: _pages[_currentIndex],
+    //   bottomNavigationBar: NavigationBar(
+    //     indicatorShape: UiConsts.shapeBoder,
+    //     selectedIndex: _currentIndex,
+    //     onDestinationSelected: (value) {
+    //       setState(() {
+    //         _currentIndex = value;
+    //       });
+    //     },
+    //     destinations: [
+    //       NavigationDestination(
+    //         icon: Icon(
+    //           _currentIndex == 0 ? Icons.home : Icons.home_outlined,
+    //         ),
+    //         label: "Home",
+    //       ),
+    //       NavigationDestination(
+    //         icon: Icon(
+    //           _currentIndex == 1
+    //               ? Icons.receipt_rounded
+    //               : Icons.receipt_outlined,
+    //         ),
+    //         label: "Transactions",
+    //       ),
+    //       NavigationDestination(
+    //         icon: Icon(
+    //           _currentIndex == 2 ? Icons.person : Icons.person_outline,
+    //         ),
+    //         label: "Profile",
+    //       ),
+    //     ],
+    //   ),
+    // );
   }
 }
